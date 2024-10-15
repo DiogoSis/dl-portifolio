f5025b95d26a0b013021646d00ff95d0dcddf828
Diogo <diogo@Note-Home.bbrouter>
2024-10-14 23:19:01 -0300

    alteração na Home

---
+++ src/components/BarTechIcons/index.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import styles from "./BarTechIcons.module.css"; 
+import iconsData from "../../static/codeIcons.json"
+
+const BarTechIcons: React.FC = () => {
+    const [icons, setIcons] = useState<{ 
+        name:string
+        url:string 
+    }[]>([])
+    useEffect(() => {
+        setIcons(iconsData)
+    }, [])
+
+  return (
+    <Box className={styles.techContainer}>
+      <Box className={styles.iconTrack}>
+        {icons.map((icon) => (
+            <img key={icon.name} src={icon.url} alt={icon.name} className={styles.techIcon} />
+        ))}
+      </Box>
+    </Box>
+  );
+};
+
+export default BarTechIcons;
